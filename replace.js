61477717a9710ffcb967d3b9b7f1a5a1a959df00
Zamechek, Shawn <zamechek@wharton.upenn.edu>
2015-09-29 15:34:54 -0400

    init

---
+++ replace.js
@@ -0,0 +1,4 @@
+ window.addEventListener("DOMContentLoaded", function() {
+ 	var re = /\s*suicide\s+/gi;
+ 	document.body.innerHTML = document.body.innerHTML.replace(re, " suicide (Suicide Hotline: 1 (800) 273-8255)");
+ });
